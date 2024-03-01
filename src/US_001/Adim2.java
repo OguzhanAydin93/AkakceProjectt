9d6165893ba402a6e788a5fea75189701afa9a8e
MertOzmen <avmertozmen93@gmail.com>
2024-03-02 00:34:56 +0300

    Kuru üzüm

---
+++ src/US_001/Adim2.java
@@ -0,0 +1,10 @@
+package US_001;
+
+public class Adim2 {
+    public static void main(String[] args) {
+
+        System.out.println("Merhaba java dünyası");
+
+        System.out.println("Üzümler bitti mi");
+    }
+}

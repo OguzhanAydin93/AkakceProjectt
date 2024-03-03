8ae06035cb1517ab4db2d2a00ab342da1aa4db70
Degisiklikler1 <megaozi9517536@gmail.com>
2024-03-03 22:07:51 +0300

    Mesaj bildirimi ve doğrulama asser'i eklendi

--- src/US_001/US_106_OguzhanAydin.java
+++ src/US_001/US_106_OguzhanAydin.java
@@ -2,11 +2,14 @@ package US_001;
 
 
 import Utlity.BaseDriver;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 
@@ -24,6 +27,7 @@ public class US_106_OguzhanAydin extends BaseDriver {
 
         Actions aksiyonlar=new Actions(driver);
 
+        bekle.until(ExpectedConditions.urlToBe("https://www.akakce.com/"));
         WebElement girisYap=driver.findElement(By.cssSelector("[id='H_rl_v8'] > :nth-child(2)"));
         girisYap.click();
 
@@ -33,6 +37,7 @@ public class US_106_OguzhanAydin extends BaseDriver {
         WebElement sifre=driver.findElement(By.cssSelector("[id='lifp']"));
         sifre.sendKeys("Testtitans_01");
 
+        bekle.until(ExpectedConditions.urlToBe("https://www.akakce.com/akakcem/giris/"));
         WebElement girisYapClick=driver.findElement(By.cssSelector("[id='lfb']"));
         girisYapClick.click();
 
@@ -42,11 +47,16 @@ public class US_106_OguzhanAydin extends BaseDriver {
 
         WebElement hesaplarim=driver.findElement(By.cssSelector("[id='HM_p_v8']+li+li+li+li>a"));
         hesaplarim.click();
+        bekle.until(ExpectedConditions.urlToBe("https://www.akakce.com/akakcem/"));
 
         WebElement mesajlar=driver.findElement(By.cssSelector("[href='/akakcem/mesajlarim/']"));
         mesajlar.click();
+        bekle.until(ExpectedConditions.urlToBe("https://www.akakce.com/akakcem/mesajlarim/"));
 
+        WebElement bildirimMsj=driver.findElement(By.cssSelector("[style='color: #777;font-size: 1.15em;font-weight: bold;text-align: center;padding-top: 30px;padding-bottom: 60px;']"));
+        System.out.println("Bildirim Mesajı = " + bildirimMsj.getText());
 
+        Assert.assertTrue("Mesaj Doğru Gözükmüyor :)",bildirimMsj.getText().contains("Listelenecek mesaj bulunamadı."));
 
         BekleVeKapat();
 

3c61a7383854ed5518f7a16e1d717cee9ef016ab
hatun <hatunyuksel.106@gmail.com>
2024-03-05 16:12:23 +0300

    sisteme giriş yapması için ilk kodları yazdım

--- src/Akakce/US102_Hatun.java
+++ src/Akakce/US102_Hatun.java
@@ -1,12 +1,23 @@
 package Akakce;
 
+import Utlity.BaseDriver;
+import Utlity.MyFunc;
 import com.google.common.annotations.VisibleForTesting;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 
-public class US102_Hatun {
+public class US102_Hatun extends BaseDriver {
     @Test
             public void test(){
+
+        driver.get("https://www.akakce.com/");
+        MyFunc.Bekle(2);
+
+        WebElement girisYap= driver.findElement(By.xpath("(//a[@href='/akakcem/giris/'])"));
+        girisYap.click();
+
 
     }
 

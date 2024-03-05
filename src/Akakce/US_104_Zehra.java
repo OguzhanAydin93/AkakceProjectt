776935f324553faabef82b7df1fd93af3e95d11a
ZGK <zehragkrl@gmail.com>
2024-03-05 10:17:14 +0300

    Giriş yap buttonu locatorı bulundu ve explicit wait eklendi.

--- src/Akakce/US_104_Zehra.java
+++ src/Akakce/US_104_Zehra.java
@@ -2,11 +2,18 @@ package Akakce;
 
 import Utlity.BaseDriver;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class US_104_Zehra extends BaseDriver {
     @Test
     public void Login(){
         driver.get("https://www.akakce.com/");
+
+        WebElement login = driver.findElement(By.xpath("(//a[text()='Giriş Yap'])[1]"));
+        login.click();
+        bekle.until(ExpectedConditions.urlContains("https://www.akakce.com/akakcem/giris/"));
 
 
 

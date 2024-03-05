3996b9b9a9a388d7eb87a26064720ff45e8d15c4
ZGK <zehragkrl@gmail.com>
2024-03-05 10:22:16 +0300

    Email ve password locatorı bulundu.

--- src/Akakce/US_104_Zehra.java
+++ src/Akakce/US_104_Zehra.java
@@ -8,13 +8,18 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class US_104_Zehra extends BaseDriver {
     @Test
-    public void Login(){
+    public void Login() {
         driver.get("https://www.akakce.com/");
 
         WebElement login = driver.findElement(By.xpath("(//a[text()='Giriş Yap'])[1]"));
         login.click();
         bekle.until(ExpectedConditions.urlContains("https://www.akakce.com/akakcem/giris/"));
 
+        WebElement email = driver.findElement(By.xpath("//input[@name='life']"));
+        email.sendKeys("zehragkrl@gmail.com");
+
+        WebElement password = driver.findElement(By.xpath("//input[@name='lifp']"));
+        password.sendKeys("Q1q2q3q4.");
 
 
     }

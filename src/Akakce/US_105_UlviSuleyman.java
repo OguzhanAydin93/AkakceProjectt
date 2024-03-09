34325cdecc82633a01a5966dfa594dd7424600c5
MertOzmen <avmertozmen93@gmail.com>
2024-03-09 22:03:19 +0300

    Class'tan,TestNG silinmiş, junit kütüphanesine göre dizayn edilmiştir.

--- src/Akakce/US_105_UlviSuleyman.java
+++ src/Akakce/US_105_UlviSuleyman.java
@@ -1,11 +1,12 @@
 package Akakce;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.testng.Assert;
-import org.testng.annotations.Test;
+
 
 import static Utlity.BaseDriver.bekle;
 import static Utlity.BaseDriver.driver;

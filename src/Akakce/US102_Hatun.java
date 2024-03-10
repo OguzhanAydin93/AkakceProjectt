5ca025571a279b483c711466274671f23b159186
hatun <hatunyuksel.106@gmail.com>
2024-03-10 07:43:53 +0300

    Son düzenlemeler yapıldı

--- src/Akakce/US102_Hatun.java
+++ src/Akakce/US102_Hatun.java
@@ -1,41 +1,33 @@
 package Akakce;
-
 import Utlity.BaseDriver;
-import Utlity.MyFunc;
-import com.google.common.annotations.VisibleForTesting;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-
 
 public class US102_Hatun extends BaseDriver {
-    @Test
-            public void test(){
+        @Test
+        public void test(){
 
-        driver.get("https://www.akakce.com/");
+            driver.get("https://www.akakce.com/");
 
-        WebElement girisYap= driver.findElement(By.xpath("(//a[@href='/akakcem/giris/'])"));
-        girisYap.click();
+            WebElement girisYap= driver.findElement(By.xpath("(//a[@href='/akakcem/giris/'])"));
+            girisYap.click();
 
-        WebElement eposta=driver.findElement(By.cssSelector("[id='life'][name='life']"));
-        eposta.sendKeys("hatunyuksel.106@gmail.com");
+            WebElement eposta=driver.findElement(By.cssSelector("[id='life'][name='life']"));
+            eposta.sendKeys("hatunyuksel.106@gmail.com");
 
-        WebElement sifre= driver.findElement(By.cssSelector("[id='lifp'][name='lifp']"));
-        sifre.sendKeys("Çaykuruüzüm1");
+            WebElement sifre= driver.findElement(By.cssSelector("[id='lifp'][name='lifp']"));
+            sifre.sendKeys("Çaykuruüzüm1");
 
-        WebElement girisbtn= driver.findElement(By.cssSelector("[id='lfb']"));
-        girisbtn.click();
+            WebElement girisbtn= driver.findElement(By.cssSelector("[id='lfb']"));
+            girisbtn.click();
 
-        WebElement hatun= driver.findElement(By.linkText("Hatun"));
-        Assert.assertTrue("Mesaj doğru değil",hatun.getText().equals("Hatun"));
-
-
-        BekleVeKapat();
+            WebElement hatun= driver.findElement(By.linkText("Hatun"));
+            Assert.assertTrue("Mesaj doğru değil",hatun.getText().equals("Hatun"));
 
 
-
+            BekleVeKapat();
 
 
     }
